eb6dc86473123f751547ee9b9c2d071fbea0fd35
NoseKnowsAll <masshellions@gmail.com>
2021-02-02 18:24:51 -0800

    Mesh script now works with strings

--- meshes/scripts/mk_naca_v1_msh.m
+++ meshes/scripts/mk_naca_v1_msh.m
@@ -1,14 +1,14 @@
-curr_dir = '~/scratch/2021/naca/meshes/scripts/';
-msh_dir  = strcat(curr_dir,'../');
+curr_dir = "~/scratch/2021/naca/meshes/scripts/";
+msh_dir  = curr_dir+"../";
 p = 3
 refinements = [0, 2, 4, 8]
 
-msh = rungmsh2msh(strcat(msh_dir,'naca_v1.geo'), strcat('-order ',int2str(p)));
+msh = rungmsh2msh(msh_dir+"naca_v1.geo", "-order "+string(p));
 mshp = mshchangep(msh, p);
 for refine = refinements
   msh = qmshbndlayer(mshp, [1], refine);
   msh = mshreorder(msh, 'weight', [2,1]);
 
-  h5fwritestruct(msh, strcat(msh_dir,'naca_v1_p',int2str(p),'_r',int2str(refine),'.h5'), dgfieldnames('msh'));
+  h5fwritestruct(msh, msh_dir+"naca_v1_p"+string(p)+"_r"+string(refine)+".h5", dgfieldnames('msh'));
 end
 
