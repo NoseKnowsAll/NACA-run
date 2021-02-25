dbe0c45e63bed091f7f1db7d770ca9a81ccdaef1
NoseKnowsAll <masshellions@gmail.com>
2021-02-24 16:36:17 -0800

    Add capability to save boundary layer elements

--- meshes/scripts/mk_naca_v2_msh.m
+++ meshes/scripts/mk_naca_v2_msh.m
@@ -2,6 +2,9 @@
 hwing = 0.049;
 hLE = 0.0099;
 
+% Should we store boundary layer elements
+store_bl = 1
+
 % Run script to create 
 curr_dir = "~/scratch/2021/naca/meshes/scripts/";
 msh_dir  = curr_dir+"../";
@@ -10,9 +13,20 @@ refinements = [0, 2, 4, 8, 12]
 msh = rungmsh2msh(msh_dir+"naca_v2.geo", "-order "+string(p), "-setnumber hwing "+string(hwing), "-setnumber hLE "+string(hLE));
 mshp = mshchangep(msh, p);
 for refine = refinements
-  msh = qmshbndlayer(mshp, [1], refine);
-  msh = mshreorder(msh, 'weight', [2,1]);
-
+  [msh, bnd_elems] = qmshbndlayer(mshp, [1], refine);
+  [msh,perm] = mshreorder(msh, 'weight', [2,1]);
+  
+  if length(bnd_elems) > 0 && store_bl
+    % Reorder boundary elements according to same permutation as overall elements got reordered
+    %[~,inv_perm] = sort(perm); % Equivalent to below, but less efficient
+    inv_perm(perm) = 1:numel(perm);
+    bnd_elems = inv_perm(bnd_elems);
+    
+    % Save boundary layer elements as .mat file
+    fid = fopen(msh_dir+"naca_v2_p"+string(p)+"_r"+string(refine)+"bl.mat", "w");
+    fprintf(fid, "%7d\n", bnd_elems);
+    fclose(fid);
+  end
   h5fwritestruct(msh, msh_dir+"naca_v2_p"+string(p)+"_r"+string(refine)+".h5", dgfieldnames('msh'));
 end
 
