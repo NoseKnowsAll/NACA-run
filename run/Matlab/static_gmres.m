425e944b25a2718d3e0fe2d1d161ee03b7d8ce85
NoseKnowsAll <masshellions@gmail.com>
2021-04-16 15:30:18 -0700

    Modify static GMRES to allow for right preconditioning

--- run/Matlab/static_gmres.m
+++ run/Matlab/static_gmres.m
@@ -7,7 +7,7 @@ function [x, iter, residuals] = static_gmres(A, b, tol, maxiter, precond, verbos
   
   % Initialization
   if nargin < 6
-    verbose = false
+    verbose = false;
     if nargin < 5
       precond = @(x) x;
     end
@@ -17,7 +17,8 @@ function [x, iter, residuals] = static_gmres(A, b, tol, maxiter, precond, verbos
   if maxiter > m
     maxiter = m;
   end
-  b0 = precond(b);
+  %b0 = precond(b); % Left preconditioning
+  b0 = b;           % Right preconditioning
   residual = norm(b0);
   tol = tol*residual;
   if verbose
@@ -32,11 +33,12 @@ function [x, iter, residuals] = static_gmres(A, b, tol, maxiter, precond, verbos
 
     % Arnoldi iteration
     if isa(A, 'function_handle')
-      vec = A(Q(:,n));
+      %vec = precond(A(Q(:,n))); % Left preconditioning
+      vec = A(precond(Q(:,n))); % Right preconditioning
     else
-      vec = A*Q(:,n);
+      %vec = precond(A*Q(:,n)); % Left preconditioning
+      vec = A*precond(Q(:,n)); % Right preconditioning
     end
-    vec = precond(vec);
     h = Q'*vec;
     vec = vec - Q*h;
     hN = norm(vec);
@@ -50,28 +52,46 @@ function [x, iter, residuals] = static_gmres(A, b, tol, maxiter, precond, verbos
     v = omegaDotH/denom;
 
     % Convergence tests
-    if (hN < tol)
+    if hN < 1e-8
+      fprintf("it: %4d, converging because hN=%f is close to 0\n", n, hN);
       break;
     end
     residual = abs(t)*residual;
+    % Efficient computation of residual. Equivalent to computing the following:
+    e1 = [1; zeros(n,1)];
+    y = H\(norm(b0)*e1);
+    residual2 = norm(norm(b0)*e1 - H*y);
+    x = Q*y;
+    x = precond(x); % Right preconditioning
+    if isa(A, 'function_handle')
+      res = b - A(x);
+    else
+      res = b - A*x;
+    end
+    %residual3 = norm(precond(res)); % Left preconditioning
+    residual3 = norm(res); % Right preconditioning
     if verbose
-      fprintf("it: %4d, res = %8.5f\n", n, residual);
+      fprintf("it: %4d, hN = %8.6f, res = %7.5f\n", n, hN, residual/norm(b0));
+      fprintf("||res1|| = %f, ||res2|| = %f, ||res3|| = %f\n", residual, residual2, residual3);
     end
     residuals = [residuals residual];
-    if (residual < tol)
+    if residual < tol
       break;
     end
 
     % Update for next iteration
-    Q = [Q vec/hN];
-    omegaN = [-t*omegaN; v];
-
+    if n < maxiter
+      Q = [Q vec/hN];
+      omegaN = [-t*omegaN; v];
+    end
+    
   end
 
   % Now that convergence has been met, retrieve solution x
   iter = n;
   e1 = [1; zeros(iter,1)];
   x = Q * ( H\(norm(b0)*e1) );
+  x = precond(x); % Right preconditioning
   
 end
 
