44feaa77e4147492646edee3f63dd658b53d746c
NoseKnowsAll <masshellions@gmail.com>
2021-09-09 16:20:07 -0700

    Testing subiteration method for MFEM. Fix bug in subiteration

--- run/Matlab/init_subiteration.m
+++ run/Matlab/init_subiteration.m
@@ -14,91 +14,117 @@ function precond = init_subiteration(A, diagA, Ms, bl_elems, b, global_precond_t
       Dinvs{it} = decomposition(Ms(:,:,it), 'lu');
     end
   end
-  precond_global = @(x) apply_global_preconditioner(Dinvs, x);
+  precond_global = @(rhs) apply_global_preconditioner(Dinvs, rhs);
 
   [A_bl, diagA_bl] = extract_suboperator(A, diagA, bl_elems);
-  b_bl = extract_subvector(b, nt, nlocal, bl_elems);
-  precond_bl = init_jacobi(A_bl, diagA_bl, b_bl);
-  precond = @(x) evaluate_subiteration(A, A_bl, b, b_bl, precond_global, precond_bl, tol, nsubiter, bl_elems, nt, nlocal, x);
-
+  
+  precond_bl = init_jacobi(diagA_bl);
+  precond = @(rhs) evaluate_subiteration(A, A_bl, diagA_bl, precond_global, precond_bl, tol, nsubiter, bl_elems, nt, nlocal, rhs);
+  
   global outer_iteration;
   outer_iteration = 0;
 end
 
-% Subiteration preconditioner is one application of P\x.
-% First apply block diagonal inverse to entire region. x2 = D\x
+% Subiteration preconditioner is one application of x = P\rhs.
+% First apply block diagonal inverse to entire region. x = D\rhs
 % Then consider just the boundary layer elements specified by bl_elems.
-% Compute boundary layer residual r_{bl} = b_{bl} - A_{bl}*x2_{bl}
-% Perform inner GMRES iteration to solve A_{bl}*e_{bl} = r_{bl} with initial guess x2_{bl} to get e_{bl}
-% Update y according to correction: y = x2 + e_{bl}
+% Compute boundary layer residual r_{bl} = rhs_{bl} - A_{bl}*x_{bl}
+% Perform inner GMRES iteration to solve A_{bl}*e_{bl} = r_{bl} with initial guess x_{bl} to get e_{bl}
+% Update x according to correction: x = x + e_{bl}
 % Note: Only valid as a preconditioner within a broader FGMRES iteration
-function y = evaluate_subiteration(A, A_bl, b, b_bl, precond_global, precond_bl, tol, nsubiter, bl_elems, nt, nlocal, x)
+function x = evaluate_subiteration(A, A_bl, diagA_bl, precond_global, precond_bl, tol, nsubiter, bl_elems, nt, nlocal, rhs)
 
-  x2 = precond_global(x);
+  x = precond_global(rhs);
+  %test_mfem(x, "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_step0.mat");
   global outer_iteration;
   outer_iteration = outer_iteration + 1;
   %if outer_iteration > 8 % TODO: ramped subiteration
   %  nsubiter = 0;
   %end
+  %nsubiter = 0; % TODO: only global preconditioner
   if nsubiter <= 0
-    y = x2;
     return;
   end
 
-  % TODO: check if convergence changes when we compute true r_bl = b_bl - A_bl(x2_bl)
+  if outer_iteration == 1
+    test_mfem(A(x), "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_Ax.mat");
+  end
+  
+  % TODO: check if convergence changes when we compute true r_bl = rhs_bl - A_bl(x_bl)
   if isa(A, 'function_handle')
-    r = b - A(x2);
+    r = rhs - A(x);
   else
-    r = b - A*x2;
+    r = rhs - A*x;
+  end
+  if outer_iteration == 1
+    test_mfem(-r, "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_step1.mat");
   end
   r_bl = extract_subvector(r, nt, nlocal, bl_elems);
-  r_global = pad_subvector(r_bl, nt, nlocal, bl_elems);
-  r_nonbl = r - r_global; % r_nonbl contains residual away from region of interest
-  b_global = pad_subvector(b_bl, nt, nlocal, bl_elems);
-  b_nonbl = b - b_global; % b_nonbl contains RHS away from region of interest
-  x2_bl = extract_subvector(x2, nt, nlocal, bl_elems);
+  if outer_iteration == 1
+    test_mfem(-r_bl, "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_step1bl.mat");
+  end
+  %r_global = pad_subvector(r_bl, nt, nlocal, bl_elems);
+  %r_nonbl = r - r_global; % r_nonbl contains residual away from region of interest
+  %rhs_bl = extract_subvector(rhs, nt, nlocal, bl_elems);
+  %rhs_global = pad_subvector(rhs_bl, nt, nlocal, bl_elems);
+  %rhs_nonbl = rhs - rhs_global; % rhs_nonbl contains RHS away from region of interest
+  x_bl = extract_subvector(x, nt, nlocal, bl_elems);
 
-  % TODO: It's not quite clear what this subiteration relative tolerance should be
-  %fprintf("Norms to consider: ||r||=%8.2e, ||r_nonbl||=%8.2e, ||b_nonbl||=%8.2e, ||r_bl||=%8.2e, ||b_bl||=%8.2e\n", ...
-	% norm(r), norm(r_nonbl), norm(b_nonbl), norm(r_bl), norm(b_bl));
-  %subtol = (norm(r_nonbl)/norm(b_nonbl)) / (norm(r_bl)/norm(b_bl));
-  %subtol = subtol/10000;
   % Solving this problem "correctly" should ensure the outer iteration is
-  % independent of the smallest bl element sizes.
-  subtol = 1e-6;
+  % independent of the smallest bl element sizes. TODO: not quite clear what subtol should be
+  subtol = 1e-3; % TODO: Pure subiteration
   %if outer_iteration > 4 % TODO: ramped subiteration
   %  subtol = tol*1e2;
   %else
   %  subtol = tol*1e-2;
   %end
-  [e_bl, subiter, residuals] = static_gmres(A_bl, r_bl, x2_bl, subtol, nsubiter, precond_bl, "right", false);
+  % TODO: Remove linear iterative solver and go back to GMRES
+
+  if outer_iteration == 1
+    test = precond_bl(r_bl);
+    test_mfem(-test, "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_precond.mat");
+    
+    test = A_bl(x_bl);
+    test_mfem(test, "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_Aloc.mat");
+  end
+
+  [e_bl, subiter, residuals] = restarted_fgmres(A_bl, r_bl, x_bl, subtol, nsubiter, nsubiter, precond_bl, true);
+  %[e_bl, subiter, residuals] = static_gmres(A_bl, r_bl, x_bl, subtol, nsubiter, precond_bl, "right", false);
+  if outer_iteration == 1
+    test_mfem(-e_bl, "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_step2.mat");
+  end
+  %iteration = init_jacobi_method(A_bl, diagA_bl, r_bl);
+  %[e_bl,subiter,residuals] = iterative_method(A_bl, r_bl, x_bl, subtol, nsubiter, iteration, true);
   fprintf("Subiteration took %d iterations to achieve ||res|| = %8.2e\n", subiter, residuals(end));
-  y = x2;
   if subiter > 0
-    y = y + pad_subvector(e_bl, nt, nlocal, bl_elems);
+    x = x + pad_subvector(e_bl, nt, nlocal, bl_elems);
+  end
+
+  if outer_iteration == 1
+    test_mfem(x, "/home/mfranco/scratch/2021/wr-les-solvers/results/temp/mult_step3.mat");
   end
 
   % TODO: Remove this check
-  if isa(A, 'function_handle')
-    r = b-A(y);
-  else
-    r = b-A*y;
-  end
-  fprintf("After preconditioner, ||r||=%8.2e\n", norm(r));
+  %if isa(A, 'function_handle')
+  %  r = rhs - A(x);
+  %else
+  %  r = rhs - A*x;
+  %end
+  %fprintf("After preconditioner, ||r||=%8.2e\n", norm(r));
   
 end
 
-% Evaluates global preconditioner to x. In this case, just block diagonal inverse x = D\x
-function y = apply_global_preconditioner(Dinvs, x)
+% Evaluates global preconditioner to rhs. In this case, just block diagonal inverse x = D\rhs
+function x = apply_global_preconditioner(Dinvs, rhs)
   nt = size(Dinvs,1);
   nlocal = Dinvs{1}.MatrixSize(2);
   
-  y = x;
-  y2 = reshape(y, nlocal, nt);
+  x = rhs;
+  x2 = reshape(x, nlocal, nt);
   for it = 1:nt
-    y2(:,it) = Dinvs{it}\y2(:,it);
+    x2(:,it) = Dinvs{it}\x2(:,it);
   end
-  y = reshape(y2, nlocal*nt, 1);
+  x = reshape(x2, nlocal*nt, 1);
 end
 
 % Extract portion of operator A corresponding to rows specified by bl_elems
@@ -107,18 +133,18 @@ function [A_bl, diagA_bl] = extract_suboperator(Atimes, diagA, bl_elems)
   diagA_bl = diagA(:,:,bl_elems);
 end
 
-% Evaluates y_bl = A_bl*x_bl, when x is a subvector
-function y_bl = evaluate_suboperator(Atimes, diagA, bl_elems, x_bl)
+% Evaluates rhs_bl = A_bl*x_bl, when x is a subvector
+function rhs_bl = evaluate_suboperator(Atimes, diagA, bl_elems, x_bl)
   nt = size(diagA,3);
   nlocal = size(diagA,2);
 
   x = pad_subvector(x_bl, nt, nlocal, bl_elems);
-  y = Atimes(x);
-  y_bl = extract_subvector(y, nt, nlocal, bl_elems);
+  rhs = Atimes(x);
+  rhs_bl = extract_subvector(rhs, nt, nlocal, bl_elems);
 end
 
 % Given a global vector x, return the portion of the vector corresponding
-% to the elements specified by bl_elems array.
+% to the elements specified by bl_elems array (not a reference).
 function x_bl = extract_subvector(x, nt, nlocal, bl_elems)
   x2 = reshape(x, nlocal, nt);
   x_bl2 = x2(:,bl_elems);
@@ -140,4 +166,26 @@ function x = pad_subvector(x_bl, nt, nlocal, bl_elems, pad_val)
   end
   x_shape(:,bl_elems) = x_bl_shape(:,:);
   x = reshape(x_shape, nlocal*nt, 1);
+end
+
+
+
+
+% Test that matvec done in Matlab is same as in MFEM C++
+function test_matvec(Atimes, b, mass_dir)
+  Ab1 = Atimes(b);
+  Ab2 = load(mass_dir+"benchmark_matvec.mat", "-ascii");
+
+  norm(b)
+  norm(Ab1)
+  norm(Ab2)
+  fprintf("diff = %8.3e\n", norm(Ab1-Ab2));
+end
+
+% Test that vector in Matlab is same as in MFEM C++
+function test_mfem(v, mfem_file)
+  v2 = load(mfem_file, "-ascii");
+
+  fprintf("  Testing MFEM: %8.3e | %8.3e\n", norm(v), norm(v2));
+  fprintf("  Testing MFEM: diff = %8.3e\n", norm(v-v2));
 end
---
+++ run/Matlab/mfem_subiteration_driver.m
@@ -0,0 +1,115 @@
+% From a set of matrices saved by Will's MFEM code, solve Ax=b with FGMRES preconditioned with subiteration
+function mfem_subiteration_driver(mfem_dt, dt, tol, nsubiter, global_precond_type, h, p)
+
+  if nargin < 7
+    p = 3;
+    if nargin < 6
+      h = 1e-3;
+      if nargin < 5
+	global_precond_type = "jacobi";
+	if nargin < 4
+	  nsubiter = 500;
+	  if nargin < 3
+	    tol = 1e-8;
+	    if nargin < 2
+	      dt = 1e-3;
+	      if nargin < 1
+		mfem_dt = 1e-3;
+	      end
+	    end
+	  end
+	end
+      else
+	if ~ismember(global_precond_type, ["jacobi", "mass_inv"])
+	  fprintf("ERROR: global preconditioner type not one of the acceptable types!\n");
+	  return;
+	end
+      end
+    end
+  end
+
+  msh_name = sprintf("aniso_p%d_h%.0e", p, h);
+  wr_dir = "/scratch/mfranco/2021/wr-les-solvers/";
+  msh_dir = wr_dir+"meshes/";
+  bl_file = msh_dir+msh_name+"bl.mat";
+  msh_file = msh_dir+msh_name+".h5";
+  results_dir = wr_dir+"results/mfem_"+msh_name+"/";
+  mass_dir = results_dir+"mass/";
+
+  fprintf("Running mfem_subiteration_driver(dt=%.1e) on mesh %s, nsubiter=%d.\n", dt, msh_name, nsubiter);
+
+  A = load(results_dir+"A.mat", "-ascii");
+  M = load(mass_dir+"Mass.mat", "-ascii");
+  b = load(mass_dir+"residual.mat", "-ascii");
+  [J, Ms, JD, Dij, b] = mfem_extract_matrices(A, M, b, msh_file, mfem_dt);
+  
+  bl_elems = freadarray(bl_file);
+  bl_elems = uint64(bl_elems(:));
+
+  %test_mfem_matrices(J, Ms, JD, Dij, b, dt);
+  %Atimes = @(x) mfem_time_dependent_jacobian(J, Ms, dt, x);
+  %test_matvec(Atimes, b, mass_dir);
+  %return;
+
+  subiteration_test(J, Ms, JD, Dij, b, bl_elems, dt, tol, nsubiter, global_precond_type);
+  
+end
+
+function [J, Ms, JD, Dij, b] = mfem_extract_matrices(A, M, b, msh_file, mfem_dt)
+  msh = h5freadstruct(msh_file);
+  nt = size(msh.p1,3);
+  nlocal = size(msh.p1,1);
+  
+  Asparse = sparse(A(:,1), A(:,2), A(:,3));
+  nlocal2 = size(Asparse,1)/nt;
+  assert(nlocal == nlocal2, "mesh (%d) does not align with matrices (%d)!", nlocal, nlocal2);
+  assert(nlocal*nt == size(Asparse,1) && nlocal*nt == size(Asparse,2), "A matrix does not fully span space");
+  Msparse = sparse(M(:,1), M(:,2), M(:,3));
+  assert(nlocal*nt == size(Msparse,1) && nlocal*nt == size(Msparse,2), "M matrix does not fully span space");
+  J = Msparse./mfem_dt - Asparse; % Also sparse
+  %b = b.*mfem_dt;
+  % Extract block-diagonal information
+  Ms = zeros(nlocal, nlocal, nt);
+  JD = zeros(nlocal, nlocal, nt);
+  Dij = zeros(nlocal*nlocal*nt,2);
+  nnz = 0;
+  for it = 1:nt
+    offset1 = (it-1)*nlocal+1;
+    offset2 = it*nlocal;
+    Ms(:,:,it) = full(Msparse(offset1:offset2, offset1:offset2));
+    JD(:,:,it) = full(J(      offset1:offset2, offset1:offset2));
+    temp_indices = repmat(offset1:offset2.', 1, nlocal);
+    Dij(nnz+1:nnz+nlocal*nlocal,1) = temp_indices(:);
+    temp_indices = repmat(offset1:offset2, nlocal, 1);
+    Dij(nnz+1:nnz+nlocal*nlocal,2) = temp_indices(:);
+    nnz = nnz + nlocal*nlocal;
+  end
+  
+end
+
+% Tests that the MFEM matrices extracted pass a sanity check
+function test_mfem_matrices(J, Ms, JD, Dij, b, dt)
+  Atimes = @(x)mfem_time_dependent_jacobian(J, Ms, dt, x);
+  diagA = Ms - dt*JD;
+  disp(size(diagA))
+  disp(Dij(1:20,:))
+  disp(diagA(Dij(1:20,1)+(Dij(1:20,2)-ones(20,1))*size(diagA,1)))
+  e=@(k,n) [zeros(k-1,1);1;zeros(n-k,1)];
+  e_1 = e(1,size(J,1));
+  first_row = Atimes(e_1);
+  e_2 = e(2,size(J,1));
+  second_row = Atimes(e_2);
+  disp([first_row(1:20) second_row(1:20)]);
+end
+
+% Test that matvec done in Matlab is same as in MFEM C++
+function test_matvec(Atimes, b, mass_dir)
+  Ab1 = Atimes(b);
+  Ab2 = load(mass_dir+"benchmark_matvec.mat", "-ascii");
+
+  norm(b)
+  norm(Ab1)
+  norm(Ab2)
+  fprintf("diff = %f\n", norm(Ab1-Ab2));
+end
+
