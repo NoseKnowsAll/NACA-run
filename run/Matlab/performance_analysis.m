20e59122d03013b06ca42bcc55ea23772e790cf5
NoseKnowsAll <masshellions@gmail.com>
2021-10-07 20:26:36 -0700

    Robust performance analysis

--- run/Matlab/performance_analysis.m
+++ run/Matlab/performance_analysis.m
@@ -1,32 +1,36 @@
 % Check performance of subiteration solver on NACA problem to analyze several different hyperparameters
-function performance_analysis(run_tests)
+function performance_analysis(run_tests, plot_tests, results_file)
 
-  if nargin < 1
-    run_tests = false;
-  end
-  
   % Initialization
-  dts = [1e-4, 1e-3, 1e-2];
-  p   = 3;
-  global_precond_type = "jacobi";
-  tol = 1e-8;
-  subtol_fudges = [1e0 1e1 1e2];
-  nsubiters = [10 20 300];
-
-  performance = zeros(length(nsubiters), length(subtol_fudges), length(dts), 3);
+  if nargin < 3
+    results_file = "/scratch/mfranco/2021/naca/run/results/Matlab/performance.mat";
+    if nargin < 2
+      plot_tests = false;
+      if nargin < 1
+	run_tests = false;
+      end
+    end
+  end
 
   if run_tests
-    % Run tests 
+    % Run tests - expensive
+    global_precond_type = "jacobi";
+    tol = 1e-8;
+    dts = [1e-4, 1e-3];
+    subtol_factors = [1e0 1e1];
+    nsubiters = [10 20 30 40 300];
+    performance = zeros(length(nsubiters), length(subtol_factors), length(dts), 3);
+    
     for idt = 1:length(dts);
       dt = dts(idt);
 
-      for ist = 1:length(subtol_fudges)
-	subtol_fudge = subtol_fudges(ist);
+      for ist = 1:length(subtol_factors)
+	subtol_factor = subtol_factors(ist);
 
 	for ins = 1:length(nsubiters);
 	  nsubiter = nsubiters(ins);
 	
-	  naca_subiteration_driver(dt, tol, nsubiter, subtol_fudge, global_precond_type);
+	  naca_subiteration_driver(dt, tol, nsubiter, subtol_factor, global_precond_type);
 	
 	  global inner_iterations;
 	  global outer_iteration;
@@ -41,20 +45,39 @@ function performance_analysis(run_tests)
     end
 
     disp(performance);
-    save("../results/Matlab/performance.mat", "performance");
+    save(results_file, "performance", "dts", "subtol_factors", "nsubiters");
   else
-    load("../results/Matlab/performance.mat");
+    % Load test results instead: dts, subtol_factors, nsubiters
+    load(results_file);
+  end
+
+  % Plot tests if interested
+  if plot_tests
+    for idt = 1:length(dts);
+      dt = dts(idt);
+
+      for ist = 1:length(subtol_factors)
+	subtol_factor = subtol_factors(ist);
+
+	% Plot cost vs nsubiters for this (dt, subtol_factor) pair
+	figure((idt-1)*length(subtol_factors)+ist);
+	plot(nsubiters, performance(:,ist,idt,3), 'r.-', 'markersize', 20);
+	title(sprintf("dt=%.1e, subtolFactor=%.1e",dt, subtol_factor));
+	xlabel("nsubiter");
+	ylabel("effective matvecs");
+      end
+    end
   end
 
   % Print results at the end
   for idt = 1:length(dts)
     dt = dts(idt);
-    for ist = 1:length(subtol_fudges)
-      subtol_fudge = subtol_fudges(ist);
+    for ist = 1:length(subtol_factors)
+      subtol_factor = subtol_factors(ist);
       for ins = 1:length(nsubiters)
 	nsubiter = nsubiters(ins);
 
-	fprintf("@ dt=%.1e, subtol_factor=%.1e, nsubiter=%3d, (inner, outer, cost) = (%d, %d, %f)\n", dt, subtol_fudge, nsubiter, performance(ins,ist,idt,1), performance(ins,ist,idt,2), performance(ins,ist,idt,3));
+	fprintf("@ dt=%.1e, subtol_factor=%.1e, nsubiter=%3d, (inner, outer, cost) = (%d, %d, %f)\n", dt, subtol_factor, nsubiter, performance(ins,ist,idt,1), performance(ins,ist,idt,2), performance(ins,ist,idt,3));
       end
     end
   end
